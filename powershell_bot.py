29b02c76fb73aa83aba8065d0fd1e4494f001a6b
Pyprohly <pyprohly@outlook.com>
2019-02-06 14:40:50 +1100

    Simplify streaming

--- powershell_bot.py
+++ powershell_bot.py
@@ -5,7 +5,6 @@
 def main():
 	import os
 	import time
-	from collections import deque
 	import logging, logging.handlers
 	from pathlib import Path
 	import praw, prawcore
@@ -42,47 +41,20 @@ def main():
 
 	subreddit = reddit.subreddit('+'.join(target_subreddits))
 
-	start_time = time.time()
-	__ = ['submission', 'inbox']
-	check_time = dict.fromkeys(__, start_time)
-	seen_deque = dict.fromkeys(__, deque(maxlen=100))
-	control_checkpoint_progression = lambda d: max(0, .5*(d - 10))
+	submission_stream = subreddit.stream.submissions(pause_after=-1, skip_existing=True)
+	inbox_stream = reddit.inbox.stream(pause_after=-1, skip_existing=True)
 
 	while True:
 		try:
-			for submission in subreddit.stream.submissions(pause_after=-1):
+			for submission in submission_stream:
 				if submission is None:
 					break
 
-				if submission.id in seen_deque['submission']:
-					logger.debug('Skip: seen item: t3_{}'.format(submission.id))
-					continue
-				if submission.created_utc < check_time['submission']:
-					if submission.created_utc < start_time:
-						logger.debug('Skip: item was submitted before bot started: t3_{}'.format(submission.id))
-					else:
-						logger.debug('Skip: timestamp was supplanted: t3_{}'.format(submission.id))
-					continue
-				check_time['submission'] += control_checkpoint_progression(submission.created_utc - check_time['submission'])
-				seen_deque['submission'].append(submission.id)
-
 				process_subsmission(submission)
 
-			for item in reddit.inbox.stream(pause_after=-1):
+			for item in inbox_stream:
 				if item is None:
 					break
-
-				if item.id in seen_deque['inbox']:
-					logger.debug('[Inbox] Skip: seen item: t4_{}'.format(item.id))
-					continue
-				if item.created_utc < check_time['inbox']:
-					if item.created_utc < start_time:
-						logger.debug('[Inbox] Skip: item was submitted before bot started: t4_{}'.format(item.id))
-					else:
-						logger.debug('[Inbox] Skip: timestamp was supplanted: t4_{}'.format(item.id))
-					continue
-				check_time['inbox'] += control_checkpoint_progression(item.created_utc - check_time['inbox'])
-				seen_deque['inbox'].append(item.id)
 
 				process_inbox_item(item)
 
